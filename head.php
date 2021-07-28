799d04adbd4bb9485529ba18fc885f8260166e1d
Danny Roux-Dufault <dRouxd@users.noreply.github.com>
2021-07-27 23:03:43 -0400

    Move header info

--- head.php
+++ head.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>CSS Template</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="style.css">
-<body>
-
 <div class="sidenav">
   <a href="#">Link</a>
   <a href="#">Link</a>
--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php include 'head.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
 <title>Index</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+<body>
 
+<?php include 'head.php'; ?>
 
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
