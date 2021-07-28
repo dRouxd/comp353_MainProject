05978cf2a688717398ba6f5a1c8d9be46748648d
Danny Roux-Dufault <dRouxd@users.noreply.github.com>
2021-07-27 23:05:09 -0400

    Update person.php

--- person.php
+++ person.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Index</title>
+<title>Person</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="style.css">
