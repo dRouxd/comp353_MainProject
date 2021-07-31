9ce8ea3ea9f9fd376113aef036ca29e7753fa5d9
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-07-31 16:55:58 -0400

    update

--- person_edit.php
+++ person_edit.php
@@ -38,12 +38,52 @@ $person =
 
 <form class="inForm">
     <p class="inForm">
-        <label class="inForm" for="">SSN: </label>
-        <input class="inForm" id="" type="text" value="test">
+        <label class="inForm" for="SSN">SSN: </label>
+        <input class="inForm" name="SSN" id="SSN" type="text" value="test">
     </p>
     <p class="inForm">
-        <label class="inForm" for="">Date of Birth: </label>
-        <input class="inForm" id="" type="text" value="test">
+        <label class="inForm" for="fname">First Name: </label>
+        <input class="inForm" name="" id="fname" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="lname">Last Name: </label>
+        <input class="inForm" name=""  id="lname" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="date_of_birth">Date of Birth: </label>
+        <input class="inForm" name="date_of_birth"  id="date_of_birth" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="medicare_card_number">Medicare Card Number: </label>
+        <input class="inForm" name="medicare_card_number" id="medicare_card_number" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="telephone">Telephone: </label>
+        <input class="inForm" name="telephone"  id="telephone" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="email">Email: </label>
+        <input class="inForm" name="email"  id="email" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="address">Address: </label>
+        <input class="inForm" name="address"  id="address" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="city">City: </label>
+        <input class="inForm" name="city"  id="city" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="">Province: </label>
+        <input class="inForm" name=""  id="" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="postal_code">Postal Code: </label>
+        <input class="inForm" name="postal_code"  id="postal_code" type="text" value="test">
+    </p>
+    <p class="inForm">
+        <label class="inForm" for="citizenship">Citizenship: </label>
+        <input class="inForm" name="citizenship"  id="citizenship" type="text" value="test">
     </p>
 </form>
 
