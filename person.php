63c4c4d957b75f1ecf4493e600ea62e49356e2f5
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-07-31 15:06:42 -0400

    update

--- person.php
+++ person.php
@@ -19,7 +19,42 @@
     <td>Medicare Number</td>
     <td>Phone</td>
     <td>Email</td>
+    <td></td>
 </th>
+
+<?php
+
+$listPeople = [[
+    "fname" => "John",
+    "lname" => "Smith",
+    "date_of_birth" => "1990-07-31",
+    "age" => 31,
+    "medicare_card_number" => "SMIJ 9007 3132",
+    "telephone" => "123-345-4567",
+    "email" => "john.smith@gmail.com"
+],
+[
+    "fname" => "Johnny",
+    "lname" => "Smithy",
+    "date_of_birth" => "1980-07-31",
+    "age" => 41,
+    "medicare_card_number" => "SMIY 8007 3132",
+    "telephone" => "987-765-6543",
+    "email" => "johnny.smithy@gmail.com"
+]];
+
+foreach($listPeople as $person)
+{
+    foreach($attribute as $k=>$v)
+    {
+        ?>
+    <td><?php %v; ?><td>    
+        <?php
+    }
+}
+
+?>
+
 </table>
 
 <?php test(); ?>
