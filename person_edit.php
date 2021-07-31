40430b40acf80fe8b15736038ee827da51526663
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-07-31 16:59:40 -0400

    update

--- person_edit.php
+++ person_edit.php
@@ -39,51 +39,51 @@ $person =
 <form class="inForm">
     <p class="inForm">
         <label class="inForm" for="SSN">SSN: </label>
-        <input class="inForm" name="SSN" id="SSN" type="text" value="test">
+        <input class="inForm" name="SSN" id="SSN" type="text" value="<?php print($person["SSN"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="fname">First Name: </label>
-        <input class="inForm" name="" id="fname" type="text" value="test">
+        <input class="inForm" name="fname" id="fname" type="text" value="<?php print($person["fname"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="lname">Last Name: </label>
-        <input class="inForm" name=""  id="lname" type="text" value="test">
+        <input class="inForm" name="lname"  id="lname" type="text" value="<?php print($person["lname"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="date_of_birth">Date of Birth: </label>
-        <input class="inForm" name="date_of_birth"  id="date_of_birth" type="text" value="test">
+        <input class="inForm" name="date_of_birth"  id="date_of_birth" type="text" value="<?php print($person["date_of_birth"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="medicare_card_number">Medicare Card Number: </label>
-        <input class="inForm" name="medicare_card_number" id="medicare_card_number" type="text" value="test">
+        <input class="inForm" name="medicare_card_number" id="medicare_card_number" type="text" value="<?php print($person["medicare_card_number"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="telephone">Telephone: </label>
-        <input class="inForm" name="telephone"  id="telephone" type="text" value="test">
+        <input class="inForm" name="telephone"  id="telephone" type="text" value="<?php print($person["telephone"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="email">Email: </label>
-        <input class="inForm" name="email"  id="email" type="text" value="test">
+        <input class="inForm" name="email"  id="email" type="text" value="<?php print($person["email"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="address">Address: </label>
-        <input class="inForm" name="address"  id="address" type="text" value="test">
+        <input class="inForm" name="address"  id="address" type="text" value="<?php print($person["address"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="city">City: </label>
-        <input class="inForm" name="city"  id="city" type="text" value="test">
+        <input class="inForm" name="city"  id="city" type="text" value="<?php print($person["city"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="">Province: </label>
-        <input class="inForm" name=""  id="" type="text" value="test">
+        <input class="inForm" name=""  id="" type="text" value="<?php print($person["province"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="postal_code">Postal Code: </label>
-        <input class="inForm" name="postal_code"  id="postal_code" type="text" value="test">
+        <input class="inForm" name="postal_code"  id="postal_code" type="text" value="<?php print($person["postal_code"]); ?>">
     </p>
     <p class="inForm">
         <label class="inForm" for="citizenship">Citizenship: </label>
-        <input class="inForm" name="citizenship"  id="citizenship" type="text" value="test">
+        <input class="inForm" name="citizenship"  id="citizenship" type="text" value="<?php print($person["citizenship"]); ?>">
     </p>
 </form>
 
