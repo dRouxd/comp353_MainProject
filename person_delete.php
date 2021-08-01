668b869783ca2df96f0387b1d497531fc906d7cd
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-08-01 00:36:26 -0400

    update

---
+++ person_delete.php
@@ -0,0 +1,43 @@
+<?php include 'mysql_queries.php'; ?>
+<?php
+
+$person_id = $_GET["person_id"];
+
+if($_GET["confirm"] == "true")
+{
+    #TODO: send the request to mysql to delete person_id
+    
+    $person_id = 1;
+    header("Location: person.php");
+    die();
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Delete Person</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+<body>
+<?php include 'head.php'; ?>
+<h3>Confirm</h3>
+
+<p>
+Are you sure you want to delete this person? <a href="person_delete.php?person_id=<?php print($person_id); ?>&confirm=true">Yes</a> <a href="person_edit.php?person_id=<?php print($person_id); ?>">No</a>
+</p>
+
+
+<?php include 'tail.php'; ?>
+
+
+
+
+
+
+
+
+
+
