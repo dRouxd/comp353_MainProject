3d1bce1da24c22c02cebfbc4b1bf5291c84803b0
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-08-01 17:20:36 -0400

    update

--- vaccination_facility_inventory_add.php
+++ vaccination_facility_inventory_add.php
@@ -25,7 +25,7 @@ $listVaccine = [
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Vaccination Facility Edit</title>
+<title>Vaccination Facility Inventory Add</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="style.css">
