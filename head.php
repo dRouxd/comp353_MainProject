5cec18c904bc8d60c01a4f135dae2dab0b482989
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-07-31 23:55:21 -0400

    update

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <div class="sidenav">
   <a href="index.php">Home</a>
   <a href="person.php">Person</a>
-  <a href="#">Link</a>
+  <a href="vaccine.php">Vaccine</a>
 </div>
 
 <div class="content">
