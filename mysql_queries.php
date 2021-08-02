d7e74b4c2fe1e0c2ee1088f21daa929134c90ceb
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-08-01 21:53:47 -0400

    update

--- mysql_queries.php
+++ mysql_queries.php
@@ -1,38 +1,40 @@
 <?php
 
-  $servername = "132.205.96.42";
-  $username = "zjc353_1";
-  $password = "DTFSK354";
+    $servername = "132.205.96.42";
+    $username = "zjc353_1";
+    $password = "DTFSK354";
 
-  function test()
-  {
+  
     // Create connection
     $conn = new mysqli($servername, $username, $password);
 
     // Check connection
-    if ($conn->connect_error) {
+    if ($conn->connect_error)
+    {
       die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
-    
-    $sql = "SELECT person_id, fname, lname FROM Person";
-    $result = $conn->query($sql);
+  
+    function test()
+    {
+        $sql = "SELECT person_id, fname, lname FROM Person";
+        $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) 
-    {
-      // output data of each row
-      while($row = $result->fetch_assoc())
-      {
-        echo "id: " . $row["person_id"]. " - Name: " . $row["fname"]. " " . $row["lname"]. "<br>";
-      }
-    } else
-    {
-      echo "0 results";
+        if ($result->num_rows > 0) 
+        {
+            // output data of each row
+            while($row = $result->fetch_assoc())
+            {
+                echo "id: " . $row["person_id"]. " - Name: " . $row["fname"]. " " . $row["lname"]. "<br>";
+            }
+        }
+        else
+        {
+        echo "0 results";
     }
 
 
     $conn = null;
-  }
+    }
 
   
 
