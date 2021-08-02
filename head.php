48a4646673c5e9ddf0edf5a378fda49e20a9d077
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-08-01 20:41:35 -0400

    update

--- head.php
+++ head.php
@@ -4,7 +4,7 @@
   <a href="vaccine.php">Vaccine</a>
   <a href="health_worker.php">Health Worker</a>
   <a href="vaccination_facility.php">Vaccination Facility</a>
-  <a href="age_group">Age Group</a>
+  <a href="age_group.php">Age Group</a>
   <a href="province_eligibility.php">Province Eligibility</a>
 </div>
 
