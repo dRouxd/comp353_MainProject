b5c968b772dbc3d5b4233197a148697ad3f4e593
woshidiyi1228 <62082095+woshidiyi1228@users.noreply.github.com>
2021-08-10 14:00:13 -0400

    Update province_eligibility_edit.php

--- province_eligibility_edit.php
+++ province_eligibility_edit.php
@@ -4,25 +4,37 @@ if(array_key_exists("save", $_POST))
 {
     #TODO: Send the updated data to the mysql
     
-    header("Location: age_group.php");
+    $provinces = getListProvince();
+
+    foreach($provinces as $p)
+    {
+        $province = $p["province"];
+	$age_group_number = $_POST[$province];
+        $sqlupdate="update Province_Eligibility set age_group_number=".$age_group_number." where province='".$province."'";
+
+        updateProvince($sqlupdate);
+    }
+    header("Location: province_eligibility.php?agn=".$age_group_number);
     die();
 }
 
 $provinces = [
-    ["province" => "AB", "ageGroupNumber" => "4"],
-    ["province" => "BC", "ageGroupNumber" => "5"],
-    ["province" => "MB", "ageGroupNumber" => "7"],
-    ["province" => "NB", "ageGroupNumber" => "4"],
-    ["province" => "NL", "ageGroupNumber" => "6"],
-    ["province" => "NS", "ageGroupNumber" => "9"],
-    ["province" => "NT", "ageGroupNumber" => "2"],
-    ["province" => "NU", "ageGroupNumber" => "4"],
-    ["province" => "ON", "ageGroupNumber" => "7"],
-    ["province" => "PE", "ageGroupNumber" => "5"],
-    ["province" => "QC", "ageGroupNumber" => "6"],
-    ["province" => "SK", "ageGroupNumber" => "8"],
-    ["province" => "YT", "ageGroupNumber" => "9"]
+    ["province" => "AB", "age_group_number" => "4"],
+    ["province" => "BC", "age_group_number" => "5"],
+    ["province" => "MB", "age_group_number" => "7"],
+    ["province" => "NB", "age_group_number" => "4"],
+    ["province" => "NL", "age_group_number" => "6"],
+    ["province" => "NS", "age_group_number" => "9"],
+    ["province" => "NT", "age_group_number" => "2"],
+    ["province" => "NU", "age_group_number" => "4"],
+    ["province" => "ON", "age_group_number" => "7"],
+    ["province" => "PE", "age_group_number" => "5"],
+    ["province" => "QC", "age_group_number" => "6"],
+    ["province" => "SK", "age_group_number" => "8"],
+    ["province" => "YT", "age_group_number" => "9"]
 ];
+
+$provinces = getListProvince();
 
 ?>
 <!DOCTYPE html>
@@ -36,7 +48,7 @@ $provinces = [
 <?php include 'head.php'; ?>
 
 <h3>Province Eligibility Edit</h3>
-<form class="inForm" action="/province_eligibility.php"  method="post">
+<form class="inForm" action="province_eligibility_edit.php"  method="post">
     <table>
         <tr>
             <th>Province</th>
@@ -49,7 +61,7 @@ foreach($provinces as $p)
 ?>
         <tr>
             <td><?php print($p["province"]); ?> </td>
-            <td><input class="inForm" name="<?php print($age["province"]); ?>" type="number" value="<?php print($p["ageGroupNumber"]); ?>" min="1" max="10"></td>
+            <td><input class="inForm" name="<?php print($p["province"]); ?>" type="number" value="<?php print($p["age_group_number"]); ?>" min="1" max="10"></td>
         </tr>
 <?php
 }
@@ -60,13 +72,3 @@ foreach($provinces as $p)
 </form>
 
 <?php include 'tail.php'; ?>
-
-
-
-
-
-
-
-
-
-
