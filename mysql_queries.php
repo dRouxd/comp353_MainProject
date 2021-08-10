afa802b7379d1ba76644d79af7711982d5daadba
woshidiyi1228 <62082095+woshidiyi1228@users.noreply.github.com>
2021-08-10 13:06:09 -0400

    Update mysql_queries.php

--- mysql_queries.php
+++ mysql_queries.php
@@ -99,40 +99,173 @@
     {
         global $conn;
         
-        $sql = "SELECT personID, firstName, lastName, DOB, age, medicareCardNumber, mobileNumber, email FROM Person";
+        $sql = "SELECT person_id, ssn, fname, lname, date_of_birth, age_group, medicare_card_number, telephone, email FROM Person";
         
         $result = $conn->query($sql);
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
-    
-    function addPerson($SSN, $fname, $lname, $dob, $cardNum, $phone, $email, $address, $postalCode, $city, $province, $citizenship)
+
+    function getPersonById($person_id)
     {
         global $conn;
         
-        #check if the postalCode is already in the database
-        $postalSql = "SELECT postalCode FROM PostalCode_Info WHERE postalCode = \"$postalCode\"";
-        $postalResult = $conn->query($postalSql);
-        if($postalResult->num_rows == 0)
-        {
-            #insert postalcode into postalcodeInfo
-            $postalCodeInsertSql = "INSERT INTO PostalCode_Info (postalCode, city, province) VALUES (\"$postalCode\", \"$city\", \"$province\")";
-            if(!$conn->query($postalCodeInsertSql))
-            {
-                print("Error: " . $conn->error);
-                return;
-            }
-        }
+        $sql = "SELECT person_id, ssn, fname, lname, date_of_birth, age_group, medicare_card_number, telephone, email, address, city, province, postal_code, citizenship FROM Person where person_id=" . $person_id;
         
-        #insert new person into database
-        $insertPersonSql = "INSERT INTO Person (SSN_Passport, firstName, lastName, DOB, medicareCardNumber, mobileNumber, email, address, postalCode, citizenship) VALUES (\"$SSN\", \"$fname\", \"$lname\", \"$dob\", \"$cardNum\", \"$phone\", \"$email\", \"$address\", \"$postalCode\", \"$citizenship\")";
-        if(!$conn->query($insertPersonSql))
-        {
-            print("Error: " . $conn->error);
-        }else
-        {
-            return $conn->insert_id;
-        }
+        $result = $conn->query($sql);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
   
+    function addPerson($sqlinsert)
+    {
+        global $conn;
+        
+        $result=$conn->query($sqlinsert);
+
+	//$sqlselect="select * from Person order by person_id DESC"
+	$sqlselect="select max(person_id) as person_id from Person";
+
+        $result = $conn->query($sqlselect);
+
+        return $result->fetch_assoc();
+
+    }
+
+    function updatePerson($sqlupdate)
+    {
+        global $conn;
+        
+        $result = $conn->query($sqlupdate);
+
+	$sqlselect="select max(person_id) as person_id from Person";
+
+        $result = $conn->query($sqlselect);
+
+        return $result->fetch_assoc();
+
+    }
+
+    function getListVaccine()
+    {
+        global $conn;
+        
+        $sql = "SELECT vaccine_id, brand, approval_date, current_status, suspended_date, description FROM Vaccine";
+        
+        $result = $conn->query($sql);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    function getVaccineById($vaccine_id)
+    {
+        global $conn;
+        
+        $sql = "SELECT vaccine_id, brand, approval_date, current_status, suspended_date, description FROM Vaccine where vaccine_id='".$vaccine_id."'";
+        
+        $result = $conn->query($sql);
+
+        return $result->fetch_assoc();
+    }
+
+    function addVaccine($sqlinsert)
+    {
+        global $conn;
+        
+        $result=$conn->query($sqlinsert);
+
+	$sqlselect="select max(vaccine_id) as vaccine_id from Vaccine";
+
+        $result = $conn->query($sqlselect);
+
+        return $result->fetch_assoc();
+
+    }
+
+    function updateVaccine($sqlupdate)
+    {
+        global $conn;
+        
+        $result = $conn->query($sqlupdate);
+
+	$sqlselect="select max(vaccine_id) as vaccine_id from Vaccine";
+
+        $result = $conn->query($sqlselect);
+
+        return $result->fetch_assoc();
+
+    }
+  
+    function getListAgeGroup()
+    {
+        global $conn;
+        
+        $sql = "SELECT age_group_number, lower_bound, upper_bound FROM age_group";
+        
+        $result = $conn->query($sql);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    function getAgeGroupByNum($age_group_number)
+    {
+        global $conn;
+        
+        $sql = "SELECT age_group_number, lower_bound, upper_bound FROM age_group where age_group_number='".$age_group_number."'";
+        
+        $result = $conn->query($sql);
+
+        return $result->fetch_assoc();
+    }
+
+    function updateAgeGroup($sqlupdate)
+    {
+        global $conn;
+        
+        $result = $conn->query($sqlupdate);
+
+	$sqlselect="select max(age_group_number) as age_group_number from age_group";
+
+        $result = $conn->query($sqlselect);
+
+        return $result->fetch_assoc();
+
+    }
+
+    function getListProvince()
+    {
+        global $conn;
+        
+        $sql = "SELECT province, age_group_number FROM Province_Eligibility order by province";
+        
+        $result = $conn->query($sql);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    function getProvinceBy($province)
+    {
+        global $conn;
+        
+        $sql = "SELECT province, age_group_number FROM Province_Eligibility where province='".$province."'";
+        
+        $result = $conn->query($sql);
+
+        return $result->fetch_assoc();
+    }
+
+    function updateProvince($sqlupdate)
+    {
+        global $conn;
+        
+        $result = $conn->query($sqlupdate);
+
+        $sqlselect = "SELECT province, age_group_number FROM Province_Eligibility";
+
+        $result = $conn->query($sqlselect);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+
+    }
+
 ?>
