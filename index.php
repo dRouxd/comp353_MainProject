ba8869784e80fda39b3b1ff1b6ed39b235dc03c0
Danny Roux-Dufault <dRouxd@users.noreply.github.com>
2021-08-12 07:22:09 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+<a class="btn btn-primary" href="question1.php">Question1</a>
+<a class="btn btn-primary" href="question2.php">Question2</a>
+<a class="btn btn-primary" href="question3.php">Question3</a>
+<a class="btn btn-primary" href="question4.php">Question4</a>
+<a class="btn btn-primary" href="question5.php">Question5</a>
+<a class="btn btn-primary" href="question6.php">Question6</a>
+<a class="btn btn-primary" href="question7-8.php">Question7-8</a>
+<a class="btn btn-primary" href="question9.php">Question9</a>
+<a class="btn btn-primary" href="question10.php">Question10</a>
+<a class="btn btn-primary" href="question11.php">Question11</a>
+</body>
+</html>
